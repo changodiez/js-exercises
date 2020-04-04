665933642720a7851ad2b6816565700b8f90698d
changodiez <joaquindiezw@gmail.com>
2020-04-04 18:22:18 +0200

    level 1 Complete

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -7,8 +7,9 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// Join two strings
+// - How many parameters does the function take? 2
+// - What is the function name? s
+// - Where is the function called? with what parameters? result
+// - What does the function return? give us a result of the funtion. 
+// - How can this function be improved? using a better name that explain what the function is doing or using + 
