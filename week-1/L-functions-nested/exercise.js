4d5dc2b7ea6877d2fbca92a07f0dbd2734f704bc
changodiez <joaquindiezw@gmail.com>
2020-04-04 18:05:14 +0200

    week1 Complete

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -1,5 +1,29 @@
+
+function getUpperCaseName(name){
+    return (name.toUpperCase());
+       
+}
+
+function greatting(great,name){
+    var UpperCaseName = getUpperCaseName(name);
+    return (great + UpperCaseName)
+}
+
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+var result1 = greatting("HELLO ", mentor1);
+var result2 = greatting("HELLO ", mentor2);
+var result3 = greatting("HELLO ", mentor3);
+var result4 = greatting("HELLO ", mentor4);
+var result5 = greatting("HELLO ", mentor5);
+
+console.log (result1);
+console.log (result2);
+console.log (result3);
+console.log (result4);
+console.log (result5);
